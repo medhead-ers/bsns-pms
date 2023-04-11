562feacac21b0480ca866a4a2144b328d9e4e5c9
David EVAN <devan.exa@gmail.com>
2023-04-11 08:03:26 +0200

    gitflow-feature-stash: cross-origin-policy

--- src/main/java/com/medhead/ers/bsns_pms/application/controller/PatientController.java
+++ src/main/java/com/medhead/ers/bsns_pms/application/controller/PatientController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class PatientController {
     @Autowired
     private PatientService patientService;
